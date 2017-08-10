fe270c758bf318dbd28f03e031d70733770229a7
AlexDgrad <alyans21@mail.ru>
2017-08-10 23:45:20 +0300

    CreateMain Activity

---
+++ app/src/main/java/com/example/alexdgrad/linkbooks/Main_activity.java
@@ -0,0 +1,14 @@
+package com.example.alexdgrad.linkbooks;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class Main_activity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
